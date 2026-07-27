2050fb9c93f2717ff955c32fc79949a3d0e321ab
grosseruser <grosseruser@users.noreply.github.com>
2026-07-28 00:12:52 +0200

    Update photobooth.lua
    Bugfix...

--- photobooth.lua
+++ photobooth.lua
@@ -1,10 +1,11 @@
 --[[
 @title PhotoBooth_x
-# verif = 2 "MoDect time before seq. (s)" [0 99]
+# verif = 2 "MoDect time before seq. (s)" [0 60]
 # shots = 3 "Shots to take in seq." [1 99]
-# delay = 2 "Delay between each shot (s)" [0 99]
-# rearm = 3 "Re-arm delay afer seq. (s)" [0 99]
+# delay = 2 "Delay between each shot (s)" [0 60]
+# rearm = 3 "Re-arm delay afer seq. (s)" [0 120]
 # gain = 3 "MoDect Threshold (1-255)" [1 255]
+# cen = 0 "MoDect only in center" [0 1]
 # snd = 1 "Sounds" [0 1]
 # led = 1 "LED" [0 1]
 # debug = 0 "Debug mode" [0 1]
@@ -17,9 +18,9 @@
 ----
 function cameraready()
     if( get_flash_mode() == 2 ) then
-	  return get_shooting()
-    else
-	  return not get_shooting() and get_flash_ready()
+		return not get_shooting()
+    else	
+		return ((not get_shooting()) and get_flash_ready())
     end
 end
 
@@ -28,14 +29,14 @@ function shootphoto()
     if (led) then set_led(9,1,30) end
     if (snd) then play_sound(4) end
     if (led) then 
-	  sleep(100)
-	  set_led(9,1,1)
+		sleep(100)
+		set_led(9,0,0)
     end
     
     if( debug > 0 ) then
-	  play_sound(1)
+		play_sound(1)
     else
-	  shoot()
+		shoot()
     end
     
     repeat sleep(50) until cameraready() == true
@@ -50,11 +51,16 @@ function continuousmotion( time )
 		motion=0
 		loop = time * 2
 		for i=1,loop do
-		    t_end = get_tick_count + 500
-		    contmotion=md_detect_motion(6,6,1,500,10,gain,debug,0,0,0,0,0,0,0,2,0)
+		    t=get_tick_count()+500
+			if (cen) then
+				contmotion=md_detect_motion(6,6,1,500,10,gain,debug,0,1,2,2,5,5,0,2,0)
+			else
+		    	contmotion=md_detect_motion(6,6,1,500,10,gain,debug,0,0,0,0,0,0,0,2,0)
+			end
 		    if( contmotion > 0 ) then
-			  motion=motion+1
-			  repeat sleep(10) until (get_tick_count>=t_end) -- wait until the scheduled time has elapsed
+				motion=motion+1
+				if (snd) then play_sound(4) end
+				repeat sleep(10) until (get_tick_count()>=t) -- wait until the scheduled time has elapsed
 		    end
 		end
 		return ( motion > time )
@@ -62,41 +68,48 @@ function continuousmotion( time )
 end
 
 -- Main Loop
+w = {false,true}
+cen=w[cen+1]
+led=w[led+1]
+snd=w[snd+1]
 set_record(1)
 rec,vid,mode=get_mode()
 if rec and not vid then
     -- remove all other display stuff
     --set_prop_str(105,1)
     repeat
-	  cls()
-	  set_led(9,0,0)
-	  print("Photobooth ready!")
-	  zones=md_detect_motion(6,6,1,600000,10,gain,1,0,0,0,0,0,0,0,2,0)
-	  if( zones > 0 ) then
-		--Turn on LED when initial motion is detected
-		if (led) then set_led(9,1,1) end
-		--Motion detected, check for "verif" seconds of continuous motion before starting series
-		--This prevents accidental triggering of photobooth if someone is just walking by
-		if ( continuousmotion(verif) ) then	
-		    --Start photo process
-		    print("Smile for the camera!")
-		    for i=1,shots do
-			  print("Picture #" .. i .. "/" .. shots)
-			  shootphoto()
-			  if (i<shots) and (delay>0) then 
-				--Checking for movement during the photoshoot is not a good idea, when everyone is supposed to be holding stil
-				for j=0,delay do sleep(1000) end
-			  end
-		    end
-		    print("All done! Next!")
-		    set_led(9,0,0)
-		    if (rearm>0) then
-			  for j=0,rearm do sleep(1000) end
-		    end
+		cls()
+		set_led(9,0,0)
+		print("Photobooth ready!")
+		if (cen) then
+	  		zones=md_detect_motion(6,6,1,600000,10,gain,debug,0,1,2,2,5,5,0,2,0)
 		else
-		    set_led(9,0,0)
+			zones=md_detect_motion(6,6,1,600000,10,gain,debug,0,0,0,0,0,0,0,2,0)
 		end
-	  end
+	  	if( zones > 0 ) then
+			if (snd) then play_sound(4) end
+			--Motion detected, check for "verif" seconds of continuous motion before starting series
+			--This prevents accidental triggering of photobooth if someone is just walking by
+			if ( continuousmotion(verif) ) then	
+				--Start photo process
+				print("Smile for the camera!")
+				for i=1,shots do
+					print("Picture #" .. i .. "/" .. shots)
+					shootphoto()
+					if (i<shots) and (delay>0) then 
+						--Checking for movement during the photoshoot is not a good idea, when everyone is supposed to be holding stil
+						for j=0,delay do sleep(1000) end
+					end
+				end
+				print("All done! Next!")
+				set_led(9,0,0)
+				if (rearm>0) then
+					for j=0,rearm do sleep(1000) end
+				end
+			else
+				set_led(9,0,0)
+			end
+	  	end
     until false
 else
     print("Camera must be in photo mode")
